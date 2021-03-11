23dd6ca75dd65acf06588d57d966a5c973f8c396
ramysawan <ramy_Sawan@hotmail.com>
2021-03-11 18:09:46 +0200

    Travelingo

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';  
+
 class MainComponent extends Component {
     state = {  }
     render() { 
         return ( 
-            <div><h1>here</h1></div>
+            <div class = "container">
+                <h1>Start your booking session NOW</h1><br/>
+                <h3>First Name:</h3>
+                <input
+                type="text"
+                value={this.state.value}
+                onChange={this.handleChange}/><br/><br/>
+                <h3>Last Name:</h3>
+                <input
+                type="text"
+                value={this.state.value}
+                onChange={this.handleChange}/><br/><br/>
+                <h3>Phone Number:</h3>
+                <input
+                type="text"
+                value={this.state.value}
+                onChange={this.handleChange}/><br/><br/>
+                <h3>Your email:</h3>
+                <input
+                type="text"
+                value={this.state.value}
+                onChange={this.handleChange}/><br/><br/>
+                <button class = "btn btn-primary">BOOK NOW</button><br/><br/>
+            </div>
          );
     }
 }
